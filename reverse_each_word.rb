d9a2a071edd64dcfd9dc2644d4daaf901a18c537
Haim D <haim11293@aol.com>
2019-06-18 19:41:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
-def reverse_each_word_using_each(sentence)
-
+def reverse_each_word_using_each(string)
+  new_array = string.split(" ")
+  return_array = []
+  new_array.each do |string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
